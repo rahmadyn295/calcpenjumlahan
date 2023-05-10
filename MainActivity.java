20322ea15d7c532eb43b4370220c73e9ceb3497c
rahmadyn295 <117925176+rahmadyn295@users.noreply.github.com>
2023-05-10 11:34:40 +0700

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,74 +1,39 @@
-package com.hp.myapplication;
+package com.latihan;
 
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
-
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-     EditText number1, number2;
-     TextView hasil;
-     Button button1,button2,button3,button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        number1 = findViewById(R.id.number1);
-        number2 = findViewById(R.id.number2);
-        hasil = findViewById(R.id.hasil);
-        button1 = findViewById(R.id.button1);
-        button2 = findViewById(R.id.button2);
-        button3 = findViewById(R.id.button3);
-        button4 = findViewById(R.id.button4);
+        TextView tv1 = findViewById(R.id.tvHASIL);
+        EditText et1 = findViewById(R.id.editText5);
+        EditText et2 = findViewById(R.id.editText6);
+        Button bt1 = findViewById(R.id.btJUMLAH);
+2222
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        bt1.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                int num1 = Integer.parseInt(number1.getText().toString());
-                int num2 = Integer.parseInt(number2.getText().toString());
-                int result = num1 + num2;
-
-                hasil.setText(String.valueOf(result));
+                String jumlah = "";
+                if (view.getId() == bt1.getId()) {
+                    int x1 = Integer.parseInt(et1.getText().toString());
+                    int x2 = Integer.parseInt(et2.getText().toString());
+                    int hasil = x1 + x2;
+                    jumlah = String.valueOf(hasil);
+                    tv1.setText(jumlah);
+                        }
+                    }
+                });
             }
-        });
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int num1 = Integer.parseInt(number1.getText().toString());
-                int num2 = Integer.parseInt(number2.getText().toString());
-                int result = num1 - num2;
+        }
 
-                hasil.setText(String.valueOf(result));
-            }
-        });
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int num1 = Integer.parseInt(number1.getText().toString());
-                int num2 = Integer.parseInt(number2.getText().toString());
-                int result = num1 * num2;
-
-                hasil.setText(String.valueOf(result));
-            }
-        });
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int num1 = Integer.parseInt(number1.getText().toString());
-                int num2 = Integer.parseInt(number2.getText().toString());
-                int result = num1 / num2;
-
-                hasil.setText(String.valueOf(result));
-            }
-        });
-    }
-}
